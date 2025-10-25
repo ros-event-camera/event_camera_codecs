f5e7caaeff11254af21a51eab7a9708e191278a2
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2025-10-25 11:59:09 -0400

    more documentation and tests

--- include/event_camera_codecs/event_processor.h
+++ include/event_camera_codecs/event_processor.h
@@ -24,10 +24,41 @@ namespace event_camera_codecs
 class EventProcessor
 {
 public:
+  /**
+   * \brief Constructor
+   */
   virtual ~EventProcessor() {}
+
+  /**
+   * \brief invoked for a change-of-contrast event
+   * \param sensor_time  time (usually in nanoseconds, depends on decoder configuration)
+   * \param ex image x-coordinate of event
+   * \param ey image y-coordinate (from top right)
+   * \param polarity 0 if OFF event (intensity decreased), 1 if ON event (intensity increased)
+   */
   virtual void eventCD(uint64_t sensor_time, uint16_t ex, uint16_t ey, uint8_t polarity) = 0;
+  /**
+   * \brief invoked for an external trigger event
+   * \param sensor_time time (usually in nanoseconds, depends on decoder configuration)
+   * \param edge indicator if rising or falling edge of trigger pulse
+   * \param id identifier of trigger source (e.g. which pin the pulse arrived on)
+   */
   virtual void eventExtTrigger(uint64_t sensor_time, uint8_t edge, uint8_t id) = 0;
+  /**
+   * \brief called after the processing of the packet has been completed, or
+   *        the time limit (until_time) has been reached.
+   */
   virtual void finished() = 0;
+  /**
+   * \brief passes raw data through from the decoder to the event processor.
+   * This can be useful for .e.g. storing the raw data.
+   * This method is called once for each processed packet, before eventCD() and
+   * eventExtTrigger() are invoked. It is called *only* when messages are fed
+   * into the decoder, *not* when raw data is already given to the decoder!
+   * \param data pointer to buffer with raw data
+   * \param len  size of buffer in bytes
+   */
+
   virtual void rawData(const char * data, size_t len) = 0;
 };
 }  // namespace event_camera_codecs
