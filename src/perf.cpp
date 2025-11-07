beff5ad2424f9dd8bed73052cd86d401697679ad
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2025-11-07 15:42:46 -0500

    returning false from eventExtTrigger() will now interrupt decode()

--- src/perf.cpp
+++ src/perf.cpp
@@ -58,7 +58,7 @@ struct EventCounter : public event_camera_codecs::EventProcessor
     numEvents++;
   }
 
-  void eventExtTrigger(uint64_t t, uint8_t, uint8_t)
+  bool eventExtTrigger(uint64_t t, uint8_t, uint8_t)
   {
 #ifdef DEBUG
     if (t < lastTime || t - lastTime > 1) {
@@ -72,6 +72,7 @@ struct EventCounter : public event_camera_codecs::EventProcessor
 #endif
     lastTime = t;
     numEvents++;
+    return (true);
   }
   void finished() {}
   void rawData(const char *, size_t) {}
